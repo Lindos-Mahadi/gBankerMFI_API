50a3738aba62f6adc073d78fb0200da5f1c96109
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-01-16 12:44:23 +0600

    Feat: Added Country Model & ViewModel
    Related Work Items: #300

---
+++ GC.MFI.Solution/GC.MFI.Models/DbModels/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.MFI.Models.DbModels
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryCode { get; set; }
+        [Required]
+        public string CountryName { get; set; }
+        [Required]
+        public string CountryShortCode { get; set; }
+        [Required]
+        public bool? isoCode3 { get; set; }
+        [Required]
+        public bool Status { get; set; }
+
+    }
+}
