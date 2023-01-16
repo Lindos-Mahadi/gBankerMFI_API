9d1892e91d6b809491c33f26539a5262e99b7f60
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-01-16 13:57:52 +0600

    Feat: Country Repository and Added GBankerDbContext
    Related Work Items: #300

--- GC.MFI.Solution/GC.MFI.DataAccess/GBankerDbContext.cs
+++ GC.MFI.Solution/GC.MFI.DataAccess/GBankerDbContext.cs
@@ -24,6 +24,7 @@ namespace GC.MFI.DataAccess
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
         public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Country> Country { get; set; }
        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
