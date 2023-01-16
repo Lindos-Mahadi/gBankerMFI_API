1ed2af9532f03f2e2d0790e2399e63ac81d6a461
arefinbizzntek <78716514+arefinbizzntek@users.noreply.github.com>
2023-01-16 16:47:22 +0600

    added new upozilla endpoint
    Related Work Items: #301

--- GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/UpozillaController.cs
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/UpozillaController.cs
@@ -1,7 +1,8 @@
 ﻿using GC.MFI.Models.DbModels;
 using GC.MFI.Services.Modules.GcMfi.Interfaces;
 using GC.MFI.Services.Modules.Security.Interfaces;
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
 {
@@ -15,6 +16,22 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
         {
             this._logger = logger;
             this._service = service;
+        }
+
+        [HttpGet]
+        [Route("GetByDistrictId")]
+        public async Task<IEnumerable<Upozilla>> GetByDistrictId(int Id)
+        {
+            try
+            {
+                var record = _service.GetMany(u => u.DistrictID == Id && u.IsActive == true);
+                return record;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex, null);
+                throw;
+            }
         }
     }
 }
