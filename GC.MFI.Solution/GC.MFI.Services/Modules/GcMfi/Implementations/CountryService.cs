85871f75571fbcbe3aafcf93592c3955e456c1e4
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-01-16 13:59:43 +0600

    Feat: Implemantation Country Service
    Related Work Items: #300

---
+++ GC.MFI.Solution/GC.MFI.Services/Modules/GcMfi/Implementations/CountryService.cs
@@ -0,0 +1,26 @@
+﻿using GC.MFI.DataAccess.Repository.Interfaces;
+using GC.MFI.Models.DbModels;
+using GC.MFI.Services.Modules.GcMfi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.MFI.Services.Modules.GcMfi.Implementations
+{
+    public class CountryService : ICountryService
+    {
+        private readonly ICountryRepository _repository;
+        public CountryService(ICountryRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public async Task<IEnumerable<Country>> GetAll(string search)
+        {
+            var countryList = await _repository.GetAll(search);
+            return countryList;
+        }
+    }
+}
