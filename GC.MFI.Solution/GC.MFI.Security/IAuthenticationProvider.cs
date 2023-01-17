58eb0faeedab443ed6cc25b2339de30ddfba2486
Rafi Samnan <rafi.samnan@bizzntek.com>
2023-01-17 17:28:59 +0600

    authorization implement

--- GC.MFI.Solution/GC.MFI.Security/IAuthenticationProvider.cs
+++ GC.MFI.Solution/GC.MFI.Security/IAuthenticationProvider.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using GC.MFI.Models;
 using GC.MFI.Security.Models;
 
 namespace GC.MFI.Security
 {
     public interface IAuthenticationProvider
     {
-         Tokens Authenticate(AuthenticationModel user);
+        Tokens Authenticate(AuthenticationModel user, AzureAD CRMConnection);
         Tokens GenerateRefreshToken(string userName);
         ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
     }
