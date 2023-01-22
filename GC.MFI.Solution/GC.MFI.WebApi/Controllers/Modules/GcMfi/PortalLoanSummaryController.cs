d95c87061ea4c2b73d1e75c2dc16800e9271e791
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-01-22 14:46:33 +0600

    feat: PortalLoanSummary Created
    Related Work Items: #227

---
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/PortalLoanSummaryController.cs
@@ -0,0 +1,39 @@
+﻿using GC.MFI.Models.DbModels;
+using GC.MFI.Services;
+using GC.MFI.Services.Modules.GcMfi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
+{
+    [Route("api/gcmfi/PortalLoanSummary")]
+    public class PortalLoanSummaryController : GCMcfinaLegacyBaseController<PortalLoanSummary>
+    {
+        private readonly ILogger<PortalLoanSummaryController> _logger;
+        private readonly IPortalLoanSummaryService _service;
+
+        public PortalLoanSummaryController(ILogger<PortalLoanSummaryController> logger, IPortalLoanSummaryService service) : base(service)
+        {
+            this._logger = logger;
+            this._service = service;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public virtual PortalLoanSummary Create(PortalLoanSummary objectToSave)
+        {
+            try
+            {
+
+                objectToSave.CreateUser = "Administrator";
+                objectToSave.CreateDate = DateTime.UtcNow;
+                _service.Create(objectToSave);
+                return objectToSave;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex, null);
+                throw;
+            }
+        }
+    }
+}
