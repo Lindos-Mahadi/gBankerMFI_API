9ac3537a553179d09f939ea66f962c9cb0b02faf
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-01-31 19:13:34 +0600

    feat : Address controller get Union & VillageList
    Related Work Items: #497

---
+++ GC.MFI.Solution/GC.MFI.Models/DbModels/VillageList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.MFI.Models.DbModels
+{
+    public class VillageList
+    {
+        public string VillageName { get; set; }
+        public string VillageCode { get;}
+
+    }
+}
---
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/AddressController.cs
@@ -0,0 +1,49 @@
+﻿using GC.MFI.Models.DbModels;
+using GC.MFI.Models.Models;
+using GC.MFI.Services.Modules.GcMfi.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
+{
+    [Route("api/GcMfi/Address")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IStoredProcedureService _storedProcedureService;
+
+        public AddressController(IStoredProcedureService storedProcedureService)
+        {
+            _storedProcedureService = storedProcedureService;
+        }
+
+        [HttpGet]
+        [Route("GetVillageListByUnion")]
+        public async Task<IEnumerable<VillageList>> GetVillageListByUnion(string SearchByCode)
+        {
+            try
+            {
+                var record = await _storedProcedureService.GetVillageListByUnion(SearchByCode);
+                return record;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpGet]
+        [Route("GetUnionListByUpozilla")]
+        public async Task<IEnumerable<UnionList>> GetUnionListByUpozilla(string SearchByCode)
+        {
+            try
+            {
+                var record = await _storedProcedureService.GetUnionListByUpozilla(SearchByCode);
+                return record;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
