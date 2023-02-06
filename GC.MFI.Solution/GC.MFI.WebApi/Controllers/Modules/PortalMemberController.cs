24b19bb2fb6cfe11187c531bb68fc63442563551
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-02-06 19:03:51 +0600

    feat : Get Education List from backend to frontend
    Related Work Items: #553

--- GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/PortalMemberController.cs
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/PortalMemberController.cs
@@ -5,6 +5,11 @@ using GC.MFI.Services.Modules.GcMfi.Interfaces;
 using GC.MFI.WebApi.Controllers.Modules.Pos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GC.MFI.WebApi.Controllers.Modules
 {
@@ -27,6 +32,27 @@ namespace GC.MFI.WebApi.Controllers.Modules
         {
             var member = await _service.GetMemberById(Id);
             return member;
+        }
+
+        [HttpGet]
+        [Route("getEducationList")]
+        public async Task<string> getEducationList()
+        {
+            var jObject = new[]
+            {
+                new { Text = "Pre-Primary", Value = "1" },
+                new  { Text = "Primary", Value = "2" },
+                new { Text = "JSC", Value = "JSC" },
+                new { Text = "Secondary", Value = "3" },
+                new { Text = "Higher Secondary", Value = "4" },
+                new { Text = "Diploma", Value = "DIP" },
+                new { Text = "Graduate", Value = "5" },
+                new { Text = "PostGraduate", Value = "6" },
+                new { Text = "Illiterate", Value = "ILL" },
+                new { Text = "Other", Value = "7" }
+            };
+
+            return JsonConvert.SerializeObject(jObject);
         }
     }
 }
