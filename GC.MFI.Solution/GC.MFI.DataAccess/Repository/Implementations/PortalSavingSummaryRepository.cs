99462f619d8d6325dfa17687158d23a33068eba7
Rafi Samnan <rafi.samnan@bizzntek.com>
2023-02-07 14:35:39 +0600

    feat: get saving summary by status and join with member and product table

--- GC.MFI.Solution/GC.MFI.DataAccess/Repository/Implementations/PortalSavingSummaryRepository.cs
+++ GC.MFI.Solution/GC.MFI.DataAccess/Repository/Implementations/PortalSavingSummaryRepository.cs
@@ -231,10 +231,52 @@ namespace GC.MFI.DataAccess.Repository.Implementations
                 TotalElement / filter.pageSize);
         }
 
-        public async Task<IEnumerable<PortalSavingSummary>> getBySavingStatus(byte type, long memberId)
+        public async Task<IEnumerable<SavingSummaryViewModel>> getBySavingStatus(byte type, long memberId)
         {
-            var getStatus = DataContext.PortalSavingSummary.Where(t => t.SavingStatus == type && t.MemberID == memberId);
-            return getStatus;
+            var savingSummary = (from pps in DataContext.PortalSavingSummary
+                                 join pl in DataContext.Product on pps.ProductID equals pl.ProductID
+                                 join m in DataContext.Member on pps.MemberID equals m.MemberID
+                                 select new SavingSummaryViewModel
+                                 {
+                                     PortalSavingSummaryID = pps.PortalSavingSummaryID,
+                                     OfficeID = pps.OfficeID,
+                                     MemberID = pps.MemberID,
+                                     MemberName = m.FirstName,
+                                     ProductID = (short)pl.ProductID,
+                                     ProductName = pl.ProductName,
+                                     CenterID = pps.CenterID,
+                                     NoOfAccount = pps.NoOfAccount,
+                                     TransactionDate = pps.TransactionDate,
+                                     Deposit = pps.Deposit,
+                                     Withdrawal = pps.Withdrawal,
+                                     Balance = pps.Balance,
+                                     InterestRate = pps.InterestRate,
+                                     SavingInstallment = pps.SavingInstallment,
+                                     CumInterest = pps.CumInterest,
+                                     MonthlyInterest = pps.MonthlyInterest,
+                                     Penalty = pps.Penalty,
+                                     OpeningDate = pps.OpeningDate,
+                                     MaturedDate = pps.MaturedDate,
+                                     ClosingDate = pps.ClosingDate,
+                                     TransType = pps.TransType,
+                                     SavingStatus = pps.SavingStatus,
+                                     EmployeeId = pps.EmployeeId,
+                                     MemberCategoryID = pps.MemberCategoryID,
+                                     Posted = pps.Posted,
+                                     IsActive = pps.IsActive,
+                                     InActiveDate = pps.InActiveDate,
+                                     CreateDate = pps.CreateDate,
+                                     CreateUser = pps.CreateUser,
+                                     OrgID = pps.OrgID,
+                                     SavingAccountNo = pps.SavingAccountNo,
+                                     Ref_EmployeeID = pps.Ref_EmployeeID,
+                                     ApprovalStatus = pps.ApprovalStatus,
+                                     MemberNomines = pps.MemberNomines,
+                                     MinLimit = pl.MinLimit,
+                                     MaxLimit = pl.MaxLimit
+                                 }).Where(t => t.SavingStatus == type && t.MemberID == memberId);
+                           
+            return savingSummary;
 
         }
 
