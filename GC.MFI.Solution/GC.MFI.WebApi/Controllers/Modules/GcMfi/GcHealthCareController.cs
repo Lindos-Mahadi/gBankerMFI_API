5b0c0268cd07a4a5d62ece243a5a6d680f09c83a
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-02-26 14:23:28 +0600

    feat : Refactor Code
    Related Work Items: #762

--- GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/GcHealthCareController.cs
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/GcHealthCareController.cs
@@ -1,15 +1,17 @@
-﻿using GC.MFI.Models.Models;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
-using System.Drawing;
-using static System.Net.Mime.MediaTypeNames;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
 {
     [Route("api/GcMfi/GcHealthCare")]
     [ApiController]
-    public class GcHealthCareController : Controller
+    public class GcHealthCareController : ControllerBase
     {
+        [HttpGet]
+        [Route("GetServiceHistoryList")]
         public IActionResult GetServiceHistoryList()
         {
             var serviceHistory = new[]
@@ -19,12 +21,14 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new  {SL = 2, ReferenceNo = "1", Date = DateTime.Now, Total = 5000, Discount = 100},
                 new  {SL = 4, ReferenceNo = "2", Date = DateTime.Now, Total = 5000, Discount = 100},
                 new  {SL = 5, ReferenceNo = "1", Date = DateTime.Now, Total = 5000, Discount = 100},
-            }.ToList();
-
-            return View(serviceHistory);
+            };
+            return Ok(serviceHistory);
+            //var ss = JsonConvert.SerializeObject(serviceHistory);
+            //return Ok(ss);
         }
-
-        public IActionResult GetMedicalHistoryList() 
+        [HttpGet]
+        [Route("GetMedicalHistoryList")]
+        public IActionResult GetMedicalHistoryList()
         {
             var medicalHistory = new[]
             {
@@ -34,10 +38,12 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new {Index = 4, MedicalHistory = "Supper", SurveyDate = DateTime.Now, ViewEdit = "edit Link" },
                 new {Index = 5, MedicalHistory = "Excellent", SurveyDate = DateTime.Now, ViewEdit = "edit Link" },
             }.ToList();
-            return View(medicalHistory);
+            return Ok(medicalHistory);
         }
-
-        public IActionResult GetBasicCheckupList() {
+        [HttpGet]
+        [Route("GetBasicCheckupList")]
+        public IActionResult GetBasicCheckupList()
+        {
 
             var basicCheckup = new[]
             {
@@ -47,9 +53,10 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new {SLNo = 4, Date = DateTime.Now, Height = 165, Wheight = 60, BMI=22.04, Waist=75, Hip=95, WH = 0.79, BTemp = 96.5, SpO2 = 99, BP="120/80", BG = "6(PBS)", BH=12, UG = "-", UP = "-", UU="+-", PR = 78, Arr = "Normal", Cho = 150, Uric=5},
                 new {SLNo = 5, Date = DateTime.Now, Height = 165, Wheight = 60, BMI=22.04, Waist=75, Hip=95, WH = 0.79, BTemp = 96.5, SpO2 = 99, BP="120/80", BG = "6(PBS)", BH=12, UG = "-", UP = "-", UU="+-", PR = 78, Arr = "Normal", Cho = 150, Uric=5}
             }.ToList();
-            return View(basicCheckup);
+            return Ok(basicCheckup);
         }
-
+        [HttpGet]
+        [Route("GetTestDocumentList")]
         public IActionResult GetTestDocumentList()
         {
             var testDocument = new[]
@@ -60,12 +67,13 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new {USL=4,UploadDate = DateTime.Now, AttachmentTitle = "breakdown", Note="Great Day Gift Ideas", Download = "Download paper", Control = "Stable" },
                 new {USL=5,UploadDate = DateTime.Now, AttachmentTitle = "breakdown", Note="Great Day Gift Ideas", Download = "Download paper", Control = "Stable" }
             }.ToList();
-            return View(testDocument);
+            return Ok(testDocument);
         }
-
+        [HttpGet]
+        [Route("GetHealthChartList")]
         public IActionResult GetHealthChartList()
         {
-            var healthChart = new[] 
+            var healthChart = new[]
             {
                 new {HCSL =1, BloodPressureSystolic = 150, BloodPressureDiastolic=100,BloodGlucos = 100,BloodHemoglobin=15, PulseRate = 100, BMI=30, Waist = 100, WasteHipRatio = 0.75, Temperature = 125, BloodChlesterol = 200,UricAcid = 6, OxygenationOfBlood=125},
                 new {HCSL =2, BloodPressureSystolic = 150, BloodPressureDiastolic=100,BloodGlucos = 100,BloodHemoglobin=15, PulseRate = 100, BMI=30, Waist = 100, WasteHipRatio = 0.75, Temperature = 125, BloodChlesterol = 200,UricAcid = 6, OxygenationOfBlood=125},
@@ -73,8 +81,10 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new {HCSL =4, BloodPressureSystolic = 150, BloodPressureDiastolic=100,BloodGlucos = 100,BloodHemoglobin=15, PulseRate = 100, BMI=30, Waist = 100, WasteHipRatio = 0.75, Temperature = 125, BloodChlesterol = 200,UricAcid = 6, OxygenationOfBlood=125},
                 new {HCSL =5, BloodPressureSystolic = 150, BloodPressureDiastolic=100,BloodGlucos = 100,BloodHemoglobin=15, PulseRate = 100, BMI=30, Waist = 100, WasteHipRatio = 0.75, Temperature = 125, BloodChlesterol = 200,UricAcid = 6, OxygenationOfBlood=125}
             }.ToList();
-            return View(healthChart);
+            return Ok(healthChart);
         }
+        [HttpGet]
+        [Route("GetPathologyList")]
         public IActionResult GetPathologyList()
         {
             var pathology = new[]
@@ -85,9 +95,10 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new {PSL =4, ReportID=12345, AnalysisDate = DateTime.Now, MicroscopicImages="Image" },
                 new {PSL =5, ReportID=12345, AnalysisDate = DateTime.Now, MicroscopicImages="Image" }
             }.ToList();
-            return View(pathology);
+            return Ok(pathology);
         }
-
+        [HttpGet]
+        [Route("GetEyeReportList")]
         public IActionResult GetEyeReportList()
         {
             var eyeReport = new[]
@@ -98,25 +109,27 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 new {ERSL =4, Date=DateTime.Now,  VisionTest="Ok", PreliminaryTest="Ok", Investigation="Good", Refraction ="NULL", FinalExam ="Pass",  GlassPrescription="None", SummaryReport="Everything OK", Prescription="Done"},
                 new {ERSL =5, Date=DateTime.Now,  VisionTest="Ok", PreliminaryTest="Ok", Investigation="Good", Refraction ="NULL", FinalExam ="Pass",  GlassPrescription="None", SummaryReport="Everything OK", Prescription="Done"}
             }.ToList();
-            return View(eyeReport);
+            return Ok(eyeReport);
         }
-
+        [HttpGet]
+        [Route("GetChildrenCheckupList")]
         public IActionResult GetChildrenCheckupList()
         {
             var childrenCheckup = new[]
             {
                 new {CCSL =1,Date=DateTime.Now, Height=165, Weight=65,  BMI=22.04, BTemp=96.5, SpO2=99, BP ="120/80", BG="6(PBS)", BH=12, UG = "-", UP = "-", PR = 78, MUAC = "MUAC", PatientColor ="Black", Pres="Pres", Action = "Action"}
             }.ToList();
-            return View(childrenCheckup);
+            return Ok(childrenCheckup);
         }
-
+        [HttpGet]
+        [Route("GetConsultationReqList")]
         public IActionResult GetConsultationReqList()
         {
             var consultationReq = new[]
             {
                 new {CRSL =1, RequestDate=DateTime.Now, ProblemsDescription ="Problems Description", ConsultancySchedule =DateTime.Now, Priority ="High", DepartmentId=258, Pres="Ok", Action ="Action"}
             }.ToList();
-            return View(consultationReq);
+            return Ok(consultationReq);
         }
     }
 }
