5e7828e06a01625ee5caa390c4e9666fba1917d1
Ashiful Arefin <ashiful.arefin@grameen.technology>
2023-03-13 18:31:17 +0600

    Loan And Saving Ledger Models
    Related Work Items: #900, #901

---
+++ GC.MFI.Solution/GC.MFI.Models/Models/LoanLedger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.MFI.Models.Models
+{
+    public class LoanLedger
+    {
+        public int SLNo { get; set; }
+        public int LoanTrxID { get; set; }
+        public int MemberID { get; set; }
+        public decimal TRLoanPaid { get; set; }
+        public decimal TRIntPaid { get; set; }
+        public decimal OnlyLoanInstt { get; set; }
+        public decimal OnlyInterestPaid { get; set; }
+        public string OfficeCode { get; set; }
+        public string OfficeName { get; set; }
+        public string CenterCode { get; set; }
+        public string CenterName { get; set; }
+        public string MemberCode { get; set; }
+        public string MemberName { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public int LoanTerm { get; set; }
+        public decimal PrincipalLoan { get; set; }
+        public decimal TotalLoanPaid { get; set; }
+        public decimal LoanBalance { get; set; }
+        public decimal LoanDue { get; set; }
+        public decimal IntCharge { get; set; }
+        public decimal TotalInterestPaid { get; set; }
+        public decimal IntDue { get; set; }
+        public decimal InstallmentDate { get; set; }
+        public string EmpName { get; set; }
+        public decimal RunningLoanBalance { get; set; }
+        public decimal RunningInterestBalance { get; set; }
+    }
+}
---
+++ GC.MFI.Solution/GC.MFI.Models/Models/SavingLedger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.MFI.Models.Models
+{
+    public class SavingLedger
+    {
+        public int SLNo { get; set; }
+        public int SavingTrxID { get; set; }
+        public decimal TRWithdrawal { get; set; }
+        public decimal TRPersonalSavings { get; set; }
+        public string OfficeCode { get; set; }
+        public string OfficeName { get; set; }
+        public string CenterCode { get; set; }
+        public string CenterName { get; set; }
+        public int MemberID { get; set; }
+        public string MemberCode { get; set; }
+        public string MemberName { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public int NoOfAccountl { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal Penalty { get; set; }
+        public decimal Deposit { get; set; }
+        public decimal MonthlyInterest { get; set; }
+        public decimal Withdrawal { get; set; }
+        public decimal Balance { get; set; }
+        public string EmpName { get; set; }
+        public decimal RunningBalance { get; set; }
+
+    }
+}
