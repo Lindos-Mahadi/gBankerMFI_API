dcd03abfffcbe7094db16f06f0963ab964c566a3
Rafi Samnan <rafi.samnan@bizzntek.com>
2023-04-17 16:34:53 +0600

    feat: specific user

--- GC.MFI.Solution/GC.MFI.Services/Modules/SignalR/ChatHub.cs
+++ GC.MFI.Solution/GC.MFI.Services/Modules/SignalR/ChatHub.cs
@@ -5,6 +5,14 @@ using Microsoft.AspNetCore.SignalR;
 
 public class ChatHub : Hub
 {
+
+    public override async Task OnConnectedAsync()
+    {
+        var connectionId = Context.ConnectionId;
+        await Clients.User(connectionId).SendAsync("Notification", "king");
+
+        await base.OnConnectedAsync();
+    }
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -12,7 +20,7 @@ public class ChatHub : Hub
 
     public async Task NotificationUpdated(List<NotificationTable> Notification)
     {
-        await Clients.User(Context.ConnectionId).SendAsync("Notification", Notification);
+        await Clients.User(Context.User.Identity.Name).SendAsync("Notification", Notification);
     }
 
 }
--- GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/ChatController.cs
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/ChatController.cs
@@ -1,8 +1,12 @@
 ﻿
+using GC.MFI.Models.DbModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Polly;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class ChatController : ControllerBase
 {
@@ -14,9 +18,31 @@ public class ChatController : ControllerBase
     }
 
     [HttpPost]
+    [Route("1")]
     public async Task<IActionResult> SendMessage(string user, string message)
     {
-      await _hubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+      var usern = HttpContext.User.Identity.Name;
+      var connectionId = HttpContext.Connection.Id;
+       // var user = Context
+      await _hubContext.Clients.User(connectionId).SendAsync("ReceiveMessage", user, message);
       return Ok();
+    }
+
+    [HttpPost]
+    [Route("2")]
+    public async Task<IActionResult> SendMessageWithConn(string connID, string message)
+    {
+        var usern = HttpContext.User.Identity.Name;
+        var connectionId = HttpContext.Connection.Id;
+        // var user = Context
+        await _hubContext.Clients.Client(connID).SendAsync("Notification", new List<NotificationTable>()
+        {
+            new NotificationTable
+            {
+                Message = "Hi there"
+            }
+        });
+        await _hubContext.Clients.All.SendAsync("Notification", new List<NotificationTable>());
+        return Ok();
     }
 }
