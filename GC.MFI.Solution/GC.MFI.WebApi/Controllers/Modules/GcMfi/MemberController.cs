f76bcfde2b76b68cec355f0434cae8d122f83a63
Rafi340 <rafisamnan340@gmail.com>
2023-06-26 15:38:38 +0600

    feat: member byte array

--- GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/MemberController.cs
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/GcMfi/MemberController.cs
@@ -2,8 +2,11 @@
 using GC.MFI.Models.DbModels;
 using GC.MFI.Models.ViewModels;
 using GC.MFI.Services;
+using GC.MFI.Services.Modules.GcMfi.Implementations;
 using GC.MFI.Services.Modules.GcMfi.Interfaces;
+using GC.MFI.Utility.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,10 +18,12 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
     {
         private readonly ILogger<MemberController> _logger;
         private readonly IMemberService _service;
-        public MemberController(ILogger<MemberController> logger, IMemberService service) : base(service)
+        private readonly IFileUploadService _uploadService;
+        public MemberController(ILogger<MemberController> logger, IFileUploadService uploadService, IMemberService service) : base(service)
         {
             _logger = logger;
             _service = service;
+            _uploadService = uploadService;
         }
 
         [HttpGet]
@@ -62,6 +67,30 @@ namespace GC.MFI.WebApi.Controllers.Modules.GcMfi
                 return await _service.GetImageByMemberID(memberId);
 
             }catch (Exception ex)
+            {
+                LogError(ex, null);
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("memberimagebytearray")]
+        public async Task<byte[]> GetImageByMemberIDByteArray()
+        {
+            try
+            {
+                var header = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]).Parameter;
+                var tokenInfo = JwtTokenDecode.GetDetailsFromToken(header);
+                var getMember = _service.GetById(tokenInfo.MemberID);
+                if (getMember.Image != null)
+                {
+                    var image = _uploadService.GetById((long)getMember.Image);
+                    return image.File;
+                }
+                return null;
+
+            }
+            catch (Exception ex)
             {
                 LogError(ex, null);
                 throw;
