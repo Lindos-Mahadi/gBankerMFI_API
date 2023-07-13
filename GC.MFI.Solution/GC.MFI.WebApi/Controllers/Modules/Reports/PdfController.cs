7aa10837fdc07fe279eeabc55616f0406598609c
Rafi Samnan <rafi.samnan@bizzntek.com>
2023-07-13 15:15:30 +0600

    feat: RepaymentScheduleReport shcdule report

--- GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/Reports/PdfController.cs
+++ GC.MFI.Solution/GC.MFI.WebApi/Controllers/Modules/Reports/PdfController.cs
@@ -7,16 +7,43 @@ namespace GC.MFI.WebApi.Controllers.Modules.Reports;
 public class PdfController : Controller
 {
     private readonly IStoredProcedureService _storedProcedureService;
+    private readonly IProductService _productService;
 
-    public PdfController(IStoredProcedureService storedProcedureService)
+    public PdfController(IStoredProcedureService storedProcedureService, IProductService productService)
     {
         _storedProcedureService = storedProcedureService;
+        _productService = productService;
     }
     // GET
-    [HttpGet]
+    [HttpGet("saving")]
     public async Task<IActionResult> Index(string officeId, string loanee1, string loanee2, string productId, string qType)
     {
         var result= await _storedProcedureService.GetSavingLedger(officeId, loanee1, loanee2, productId, qType);
         return View(result);
     }
+
+    [HttpGet("loan")]
+    public async Task<IActionResult> RepaymentScheduleReport(int officeID, int memberId, int productId, int loanTerm)
+    {
+        short id = Convert.ToInt16(productId);
+        var getProduct = _productService.GetByIdShort(id);
+        switch (getProduct.InterestCalculationMethod)
+        {
+            case "A":
+                var res = await _storedProcedureService.GetRepaymentScheduleAE(officeID, memberId, productId, loanTerm);
+                return View(res);
+            case "D":
+                var res1 = await _storedProcedureService.GetRepaymentScheduleD(officeID, memberId, productId, loanTerm);
+                return View(res1);
+            case "F":
+                var res3 = await _storedProcedureService.GetRepaymentScheduleF(officeID, memberId, productId, loanTerm);
+                return View(res3);
+            default:
+                return null;
+
+        }
+       // var result = await _storedProcedureService.GetRepaymentScheduleF(officeID, memberId, productId, loanTerm);
+       // return View(result);
+    }
+
 }
